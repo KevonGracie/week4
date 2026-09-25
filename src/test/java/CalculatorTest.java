14eef4ec0615c5312bd3794eb8eddb84f5d052a0
Kevon Gracie <j01984472@jwu.edu>
2026-09-25 18:10:31 -0400

    Add Maven project and JUnit test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(10, 25);
+
+        assertEquals(35, result);
+    }
+}
